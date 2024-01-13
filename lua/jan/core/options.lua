63b62bee19f9f8181be3d422ef17ac6b7a398672
Jan Biasi <janbiasi@users.noreply.github.com>
2024-01-13 19:22:57 +0100

    feat: configure undo, scrolloff and column signing

--- lua/jan/core/options.lua
+++ lua/jan/core/options.lua
@@ -3,4 +3,12 @@ local opt = vim.opt -- for conciseness
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
 vim.opt.colorcolumn = "120"
